aa64ab68f1aa5f1e33b0e3291e71f01deb2bd45a
Vitor <vitor.sofiati@live.com>
2024-03-28 20:36:54 -0300

    dropdown menu refatorado

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -5,7 +5,7 @@ export default class ScrollAnima {
 		this.sections = document.querySelectorAll(sections);
 		this.windowMetade = window.innerHeight * 0.6;
 
-		this.checkDistance = debounce(this.checkDistance.bind(this), 50);
+		this.checkDistance = debounce(this.checkDistance.bind(this), 20);
 	}
 
 	// Pega a distância de cada item em relação ao topo.
@@ -21,7 +21,6 @@ export default class ScrollAnima {
 
 	// Compara a distância (até o topo) de cada objeto com o scroll do site
 	checkDistance() {
-		console.log('teste');
 		this.distance.forEach((item) => {
 			if (window.scrollY > item.offset) {
 				item.element.classList.add('ativo');
