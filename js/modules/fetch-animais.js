4d369ee88d101b10651d24f94d05e4d99a5b3bf6
Vitor <vitor.sofiati@live.com>
2024-03-28 12:59:40 -0300

    Fetch animais refatorado

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,26 +1,9 @@
 import AnimaNumeros from './anima-numeros.js';
 
-export default function initFetchAnimais() {
-	async function fetchAnimais(url) {
-		try {
-			const animaisResponse = await fetch(url);
-			const animaisJSON = await animaisResponse.json();
-			const numerosGrid = document.querySelector('.numeros-grid');
-			animaisJSON.forEach((animal) => {
-				const divAnimal = createAnimal(animal);
-				numerosGrid.appendChild(divAnimal);
-			});
-			const animaNumeros = new AnimaNumeros(
-				'[data-numero]',
-				'.numeros',
-				'ativo'
-			);
-			animaNumeros.init();
-		} catch (erro) {
-			console.log(erro);
-		}
-	}
+export default function fetchAnimais(url, target) {
+	const numerosGrid = document.querySelector(target);
 
+	// cria a div com o total de animais
 	function createAnimal(animal) {
 		const div = document.createElement('div');
 		div.classList.add('numero-animal');
@@ -28,5 +11,31 @@ export default function initFetchAnimais() {
 		return div;
 	}
 
-	fetchAnimais('./animaisapi.json');
+	// executa createAnimal e insere no DOM
+	function preencherAnimais(animal) {
+		const divAnimal = createAnimal(animal);
+		numerosGrid.appendChild(divAnimal);
+	}
+
+	function animaAnimaisNumeros() {
+		const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+		animaNumeros.init();
+	}
+
+	// Puxa os animais através de um arquivo json e cria cada animal usando createAnimal
+	async function criarAnimais() {
+		try {
+			// fetch espera a resposta e transforma em JSON
+			const animaisResponse = await fetch(url);
+			const animaisJSON = await animaisResponse.json();
+
+			// Preencher e animar estão aqui dentro pois a função é assíncrona, são ativadas só após a transformação do JSON
+			animaisJSON.forEach((animal) => preencherAnimais(animal));
+			animaAnimaisNumeros();
+		} catch (erro) {
+			console.log(erro);
+		}
+	}
+
+	return criarAnimais();
 }
