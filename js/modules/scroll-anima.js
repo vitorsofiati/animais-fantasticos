76c3717d152a562eab4e832ad3b2364809fe6829
Vitor <vitor.sofiati@live.com>
2024-03-29 11:26:52 -0300

    acertado o caminho da API, alterado o timer do debounce em scroll-anima.js

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -5,7 +5,7 @@ export default class ScrollAnima {
 		this.sections = document.querySelectorAll(sections);
 		this.windowMetade = window.innerHeight * 0.6;
 
-		this.checkDistance = debounce(this.checkDistance.bind(this), 20);
+		this.checkDistance = debounce(this.checkDistance.bind(this), 10);
 	}
 
 	// Pega a distância de cada item em relação ao topo.
